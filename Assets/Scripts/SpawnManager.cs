9103dac6414e7eb0bbfeca2fdca368a241408a3b
eronmoran <ermoran@s.sfusd.edu>
2023-02-08 10:21:17 -0800

    Animals Spawn press "S"

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class SpawnManager : MonoBehaviour
 {
+    public GameObject[] animalPrefabs;
     // Start is called before the first frame update
     void Start()
     {
