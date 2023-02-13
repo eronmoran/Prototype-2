3ce6dc819d2575f8e26ac4cdd4b1efcadd10303e
eronmoran <ermoran@s.sfusd.edu>
2023-02-13 14:37:29 -0800

    COMPLETE
    Everything is done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,16 +20,16 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //Keep the player in bounds
-        if (transform.position.x < -xRange)
+if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
 
         if (transform.position.x > xRange)
         {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+    transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }       
-        horizontalInput = Input.GetAxis("Horizontal");
+    horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     
         if (Input.GetKeyDown(KeyCode.Space))
